dc3f369936dbd7b14ddaf24bee67cd7cd3ba6004
Craig Ambrose <craig@craigambrose.com>
2024-10-22 17:55:27 +1100

    fix for zero dollars

--- app/models/monthly_scheme_aggregate.rb
+++ app/models/monthly_scheme_aggregate.rb
@@ -25,7 +25,7 @@ class MonthlySchemeAggregate < ApplicationRecord
 
   def liquidity
     #raise "#{fbo_funds_total.inspect} / #{te_total.inspect}"
-    fbo_funds_total / te_total
+    divide_or_default fbo_funds_total, te_total, 1.0
   end
 
   def stream_dollar_value(stream_id)
@@ -48,14 +48,19 @@ class MonthlySchemeAggregate < ApplicationRecord
   def stream_ratio(stream_id)
     validate_stream_id(stream_id)
 
-    stream_dollar_value(stream_id) / total_stream_dollar_value
+    divide_or_default(stream_dollar_value(stream_id), total_stream_dollar_value)
   end
 
   def te_value_ratio
-    te_total_value / te_total
+    divide_or_default(te_total_value, te_total)
   end
 
   private
+
+  def divide_or_default(numerator, denominator, default = 0)
+    return default if denominator.zero?
+    numerator / denominator
+  end
 
   def validate_stream_id(stream_id)
     raise "Invalid stream_id: #{stream_id}" unless STREAM_IDS.include?(stream_id)
